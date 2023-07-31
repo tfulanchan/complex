212bc45cc3ff73fdf34d665cc7a7e8ba371f5505
tfulanchan <60479975+tfulanchan@users.noreply.github.com>
2023-07-31 18:49:51 +0300

    Update index.js
    import useDocusaurusContext from '@docusaurus/useDocusaurusContext';

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,10 +2,12 @@ import React from "react";
 import clsx from "clsx";
 import Layout from "@theme/Layout";
 import Link from "@docusaurus/Link";
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import styles from "./index.module.css";
 import HomepageFeatures from "../components/HomepageFeatures";
 //
 function HomepageHeader() {
+  const {siteConfig} = useDocusaurusContext();
   return (
     <header className={clsx("hero--primary", styles.heroBanner)}>
       <div className="container">

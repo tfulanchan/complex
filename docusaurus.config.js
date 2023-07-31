a3c24c267619b0abcddf7dd505f7d2cd70239d43
tfulanchan <60479975+tfulanchan@users.noreply.github.com>
2023-07-31 13:59:14 +0300

    Update docusaurus.config.js
    /

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
   url: 'https://accomplished.vercel.app/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: 'https://accomplished.vercel.app/',
+  baseUrl: '/',
   
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',

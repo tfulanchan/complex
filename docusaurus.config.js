d401452702e6ad3900d02bc7e89b2c62ee4e6ed8
tfulanchan <60479975+tfulanchan@users.noreply.github.com>
2023-08-22 00:37:25 +0300

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -116,18 +116,18 @@ const config = {
         darkTheme: darkCodeTheme,
       },
     }),
-    themes: [
-      [
-        "@easyops-cn/docusaurus-search-local",
-        {
-          hashed: true,
-          language: ["en", "zh"],
-          highlightSearchTermsOnTargetPage: true,
-          explicitSearchResultPath: true,
-          // indexDocs: false,
-        },
-      ],
-    ],
+    // themes: [
+    //   [
+    //     "@easyops-cn/docusaurus-search-local",
+    //     {
+    //       hashed: true,
+    //       language: ["en", "zh"],
+    //       highlightSearchTermsOnTargetPage: true,
+    //       explicitSearchResultPath: true,
+    //       // indexDocs: false,
+    //     },
+    //   ],
+    // ],
 };
 
 module.exports = config;

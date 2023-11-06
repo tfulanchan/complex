6ccbb19bae0af7bafa85f2f4e26108308c09b0b3
tfulanchan <60479975+tfulanchan@users.noreply.github.com>
2023-11-06 20:59:56 +0300

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,7 +12,7 @@ function HomepageHeader() {
   return (
     <header className={clsx("hero--primary", styles.heroBanner)}>
       <div className="container">
-        <img src='img/cropped-kfgqpc-logo2-1.svg' />
+        <img src='img/logo.svg' />
         <h1 className="hero__title">{siteConfig.title}</h1>
         <h2 className="hero__subtitle">{siteConfig.tagline}</h2>
         <p className="hero__subtitle">{siteConfig.customFields.more}</p>

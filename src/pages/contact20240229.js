f3aaad171767c305c0b45fd0f9b6c60f3481b14e
tfulanchan <60479975+tfulanchan@users.noreply.github.com>
2024-02-29 03:30:00 -0500

    聯繫站長

--- src/pages/contact20240229.js
+++ src/pages/contact20240229.js
@@ -18,7 +18,7 @@ function sendEmail(e) {
         .then((result) => {
             alert("您的留言已提交。 您現在可以離開此頁面。");
             console.log(result.text);
-            return go;
+            return;
         }, (error) => {
             console.log(error.text);
         });
@@ -36,20 +36,22 @@ export default function Hello() {
                         <div>
                             <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">姓名</label>
                             <input type="text" id="name" className="mb-2 shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
-                                name="name"
-                                placeholder="您的姓名" required />
+                                name="name" placeholder="您的姓名" required />
                         </div>
                         <div>
                             <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">電郵地址</label>
-                            <input type="email" id="email" className="mb-2 shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="email@address.com" required />
+                            <input type="email" id="email" className="mb-2 shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" 
+                            name="email" placeholder="email@address.com" required />
                         </div>
                         <div>
                             <label htmlFor="subject" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">標題</label>
-                            <input type="text" id="subject" className="mb-2 block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="您留言的主旨" required />
+                            <input type="text" id="subject" className="mb-2 block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" 
+                            name="subject" placeholder="您留言的主旨" required />
                         </div>
                         <div className="sm:col-span-2">
                             <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">信息</label>
-                            <textarea id="message" rows="6" className="mb-2 block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="您的留言..." required></textarea>
+                            <textarea id="message" rows="6" className="mb-2 block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" 
+                            name="message" placeholder="您的留言..." required></textarea>
                         </div>
                         <button type="submit" onSubmit={sendEmail} className="mb-4 py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-green-900 hover:bg-green-700 focus:bg-green-500">
                             提交

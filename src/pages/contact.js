af84331fd93a6cdb09049b4035bda299c43d26d7
tfulanchan <60479975+tfulanchan@users.noreply.github.com>
2024-03-04 22:49:58 +0300

    Update contact.js

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -12,7 +12,10 @@ const publicKey = '5r2tCrDs5VYOZxpfF';
 const contactForm = () => {
     const history = useHistory();
     const [showRedirectMessage, setShowRedirectMessage] = useState(false);
-
+    const onSubmit = (event) => {
+        event.preventDefault();
+        captchaRef.current.execute();
+    };
     const sendEmail = (e) => {
         e.preventDefault();
         console.log('留言已提交');
@@ -50,7 +53,7 @@ const contactForm = () => {
                     <div className="py-3 lg:py-5 px-4 mx-auto max-w-screen-md">
                         {/* <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">お問い合わせ</h2> */}
                         <p className="mb-2 lg:mb-4 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">歡迎作出跟技術問題或編輯誤差有關的留言</p>
-                        <form id="formID" onSubmit={sendEmail} className="space-y-8a">
+                        <form id="formID" onSubmit={onSubmit} className="space-y-8a">
                             <div>
                                 <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">姓名</label>
                                 <input type="text" id="name" className="mb-2 shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"

5e1b7e7a8bebaa336c30a0e3077a9adcca31492c
tfulanchan <60479975+tfulanchan@users.noreply.github.com>
2024-03-16 10:35:09 +0300

    Update interactive.js

--- src/pages/interactive.js
+++ src/pages/interactive.js
@@ -34,9 +34,9 @@ export default function SearchBarWrapper() {
                             anon_key={customFields.mendableAnonKey}
                             style={{ accentColor: '#179C54', darkMode: false }}
                             hintText="安拉為何創造人類？"
-                            hintQuestions={['安拉為何創造人類？', '爾撒（耶穌）是安拉的兒子嗎？', '為什麼鼓勵結婚？', '結婚的條件是什麼？','總結《離婚章》']}
+                            hintQuestions={['安拉為何創造人類？', '爾撒（耶穌）是安拉的兒子嗎？', '總結關於結婚和離婚的規定','總結《離婚章》']}
                             dialogPlaceholder="安拉為何創造人類？"
-                            showSimpleSearch={true}
+                            showSimpleSearch={true}總結關於結婚和離婚的規定
                             messageSettings={{
                                 prettySources: true,
                                 // sourcesFirst: true
